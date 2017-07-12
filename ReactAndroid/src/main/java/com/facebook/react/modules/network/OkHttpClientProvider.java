61a6202b75cb8f1277d452fbdb88a61a677cbd5f
Martin Bielik <mx.bielik@gmail.com>
2017-07-12 16:27:33 +0200

    http client to trust all certificates

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
@@ -19,6 +19,15 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nullable;
 
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.TlsVersion;
@@ -53,7 +62,43 @@ public class OkHttpClientProvider {
       .writeTimeout(0, TimeUnit.MILLISECONDS)
       .cookieJar(new ReactCookieJarContainer());
 
-    return enableTls12OnPreLollipop(client).build();
+    return trustAllCertificates(client).build();
+  }
+
+  public static OkHttpClient.Builder trustAllCertificates(OkHttpClient.Builder client) {
+    try {
+      // Create a trust manager that does not validate certificate chains
+      final TrustManager[] trustAllCerts = new TrustManager[] {
+        new X509TrustManager() {
+          @Override
+          public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+          }
+          @Override
+          public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+          }
+          @Override
+          public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return new java.security.cert.X509Certificate[0];
+          }
+        }
+      };
+      // Install the all-trusting trust manager
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+      client.sslSocketFactory(sslSocketFactory);
+      client.hostnameVerifier(new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      });
+
+    } catch (Exception exc) {
+      FLog.e("OkHttpClientProvider", "Error while enabling trust to all certificates", exc);
+    }
+    return client;
   }
 
   /*
